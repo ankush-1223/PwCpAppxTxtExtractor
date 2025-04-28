44d08fc483b36f0c6dd90037e9f6282c11354d74
ankush-1223 <rdxankush23@gmail.com>
2025-04-29 03:08:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "28201702"
 api_hash = "31c9bbed9c688b89736d94da7e89653b"
-bot_token = "8184860937:AAFFJMH1X-NPNCftz7c-KB6jOVRW3fhO-pY"
+bot_token = "8184860937:AAHwv-3je_aCtOTk3jZkHuRCTKTAKCdQlCk"
 auth_users = [7526345865]
