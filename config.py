8770b4dac0909bd84c02683e6018bac5cf728ba8
ankush-1223 <rdxankush23@gmail.com>
2025-04-29 03:02:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "28201702"
 api_hash = "31c9bbed9c688b89736d94da7e89653b"
-bot_token = "8184860937:AAGDfMRvw_4ME8Wm4Jnxl8ekMxVz93WeAek"
+bot_token = "8184860937:AAFFJMH1X-NPNCftz7c-KB6jOVRW3fhO-pY"
 auth_users = [7526345865]
